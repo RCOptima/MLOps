2ec191abdf95ca838739cc4763331e9ee6737005
ross.campbell@optimapartners.co.uk <ross.campbell@optimapartners.co.uk>
2026-08-04 14:10:45 +0000

    add notebook test

--- steps/ingest.py
+++ steps/ingest.py
@@ -1,4 +1,7 @@
+from pyspark.sql import SparkSession
 
-def ingest(table_name):
+def ingest(spark, table_name):
+    """Load a Unity Catalog table as a pandas DataFrame."""
+    # spark = SparkSession.builder.getOrCreate()   
     df = spark.table(table_name).toPandas()
     return df
